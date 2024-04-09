5a73161548ca668078170568205a3aa41f0e5cc4
Terry Kim <terrybk@uci.edu>
2024-04-08 19:20:34 -0700

    Part A is finished

--- PartA.py
+++ PartA.py
@@ -1,6 +1,6 @@
 import sys
 from collections import defaultdict
-
+from collections import deque
 
 class Token:
 
@@ -12,21 +12,45 @@ class Token:
         #function takes in a file object and then returns a list of tokens
         #the input paramters is a .txt file and is a string
 
-        with open(file, 'r') as file:
-            for line in file:
-                for word in line.strip():
-                    if word.isalnum():
-                        self.tokens[word.lower()] += 1
+        for line in sys.stdin:
+            for word in line.strip().split():
+                temp = deque()
+                temp.append(word)
+                while temp:
+                    word = temp[0]
+                    i = 0
+                    while i < len(word):
+                        letter = word[i]
+                        if not letter.isalnum():
+                            self.tokens[word[:i]] += 1
+                            temp.append(word[i+1:])
+                            break
+                        i += 1
+                    else:
+                        if word != "":
+                            self.tokens[word.lower()] += 1
+                    temp.popleft()
+                    
+
 
     def computeWordFrequencies(self):
         return self.tokens.items()
 
 
-    def token_print(self):
+    def tprint(self):
         #function prints out the word frequencies
 
-        s = self.computeWordFrequencies()
+        sorted_word_count = dict(sorted(self.computeWordFrequencies(), key=lambda x: (-x[1], x[0])))
 
-        return s.sort(key = lambda x: (-x[1], x[0]))
+        for word, count in sorted_word_count.items():
+            print(f"{word}: {count}")
+
+
+if __name__ == "__main__":
+    print("hello")
+    test = Token()
+    test.tokenize("testA.txt")
+    test.tprint()
+    print("made it this far")
 
 
