be0958d53286cfea4742afc0546dda8bf0895fbf
Terry Kim <terrybk@uci.edu>
2024-04-10 23:09:45 -0700

    Revised the file processing function in partA

--- PartA.py
+++ PartA.py
@@ -11,28 +11,27 @@ class Token:
         #function takes in a file object and then returns a list of tokens
         #the input paramters is a .txt file and is a string
         temp = deque()
-        for line in sys.stdin:
-            for word in line.strip().split():
-                temp.clear()
-                temp.append(word)
-                while temp:
-                    word = temp[0].lower()
-                    i = 0
-                    while i < len(word):
-                        letter = word[i]
-                        if not ((ord(letter) >= 48 and ord(letter) <= 57) or \
-                            (ord(letter) >= 97 and ord(letter) <= 122)):
-                            if word[:i] != "":
-                                self.tokens[word[:i]] += 1
-                            temp.append(word[i+1:])
-                            break
-                        i += 1
-                    else:
-                        if word != "":
-                            self.tokens[word] += 1
-                    temp.popleft()
-                    
-
+        with open(file, "r") as f:
+            for line in f:
+                for word in line.strip().split():
+                    temp.clear()
+                    temp.append(word)
+                    while temp:
+                        word = temp[0].lower()
+                        i = 0
+                        while i < len(word):
+                            letter = word[i]
+                            if not ((ord(letter) >= 48 and ord(letter) <= 57) or \
+                                (ord(letter) >= 97 and ord(letter) <= 122)):
+                                if word[:i] != "":
+                                    self.tokens[word[:i]] += 1
+                                temp.append(word[i+1:])
+                                break
+                            i += 1
+                        else:
+                            if word != "":
+                                self.tokens[word] += 1
+                        temp.popleft()
 
     def computeWordFrequencies(self):
         return self.tokens.items()
@@ -48,10 +47,12 @@ class Token:
 
 
 if __name__ == "__main__":
-    print("hello")
     test = Token()
-    test.tokenize("testA.txt")
+    if len(sys.argv) <= 1:
+        print("please input a file")
+    else:
+        for i in sys.argv[1:]:
+            test.tokenize(i)
     test.print()
-    print("made it this far\n")
-
+    print("made it this far")
 
